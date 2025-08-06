10e22b7153a896999b26bebe958579fc003d752f
soyalattee <uyyu1734@gmail.com>
2025-08-06 23:30:12 +0900

    refactor: init 데이터 파일 분리
    coupons 와 products의 초기값 데이터를 별도 파일로 app.tsx에서 분리한다

---
+++ src/basic/datas/coupons.ts
@@ -0,0 +1,17 @@
+import { Coupon } from '../../types';
+
+// 초기 데이터
+export const initialCoupons: Coupon[] = [
+  {
+    name: '5000원 할인',
+    code: 'AMOUNT5000',
+    discountType: 'amount',
+    discountValue: 5000,
+  },
+  {
+    name: '10% 할인',
+    code: 'PERCENT10',
+    discountType: 'percentage',
+    discountValue: 10,
+  },
+];
