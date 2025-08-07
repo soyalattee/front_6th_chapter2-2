0580ed58e490708ffab0b152ee69c7f909bbe6bf
soyalattee <uyyu1734@gmail.com>
2025-08-08 05:26:55 +0900

    chore: svg를 컴포넌트로 사용하기 위한 모듈 및 설정 추가
    - vite-plugin-svgr 추가
    - svgr 타입 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import svgr from 'vite-plugin-svgr';
 export default mergeConfig(
   defineConfig({
-    plugins: [react()],
+    plugins: [react(), svgr()]
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
       setupFiles: './src/setupTests.ts'
-    },
+    }
   })
-)
+);
