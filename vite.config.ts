541adcf994b0202b8aac1acaf939b32272e68b21
soyalattee <uyyu1734@gmail.com>
2025-08-08 05:40:32 +0900

    rollback: svgr 라이브러리 제거
    - svg 아이콘들 tsx로 쓸 예정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import svgr from 'vite-plugin-svgr';
+
 export default mergeConfig(
   defineConfig({
-    plugins: [react(), svgr()]
+    plugins: [react()]
   }),
   defineTestConfig({
     test: {
